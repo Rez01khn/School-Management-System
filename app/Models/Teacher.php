6e1c6c49d00f3d9112f9332bfca3cfbbabecc9ca
Rez01khn <rez01khan07@gmail.com>
2026-04-13 15:46:15 +0600

    Start commiting

---
+++ app/Models/Teacher.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Teacher extends Model
+{
+     use HasFactory;
+        public $timestamps = false;
+        protected $primarykey = 'tenant_id';
+    protected $fillable = [
+        'tenant_id',
+        'address',
+    ];
+}

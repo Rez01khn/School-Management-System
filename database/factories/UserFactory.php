2d31583dbd37105c58ec7c392b81a30f0674e8aa
Rez01khn <rez01khan07@gmail.com>
2026-05-10 12:22:41 +0600

    solve

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'tenant_id' => \App\Models\Tenant::factory(), 
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
